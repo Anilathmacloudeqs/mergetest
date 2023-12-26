dc738c944e9135172c5539469f52c9ef1fbcef47
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-26 19:07:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("hello world")
+import requests
+import base64
+import os
+
+def push_file_to_branch(username, repository, source_branch, destination_branch, file_path, commit_message):
+    source_api_url = f'https://api.github.com/repos/{username}/{repository}/contents/{file_path}?ref={source_branch}'
+    destination_api_url = f'https://api.github.com/repos/{username}/{repository}/contents/{file_path}?ref={destination_branch}'
+
+    access_token = os.environ['PAT_TOKEN']
+    headers = {'Authorization': f'token {access_token}'}
+
+    source_file_response = requests.get(source_api_url, headers=headers)
+    print(source_api_url)
+
+    if source_file_response.status_code == 200:
+        source_file_content = source_file_response.json()
+        source_commit_sha = source_file_content['sha']
+
+        decoded_content = base64.b64decode(source_file_content['content']).decode()
+
+        payload = {
+            'message': commit_message,
+            'content': base64.b64encode(decoded_content.encode()).decode(),
+            'branch': destination_branch,
+            'sha': source_commit_sha
+        }
+
+        response = requests.put(destination_api_url, headers=headers, json=payload)
+
+        if response.status_code == 201 or response.status_code == 200:
+            print(f"File '{file_path}' successfully pushed to the '{destination_branch}' branch.")
+        else:
+            print(f"Error: Unable to push file. Status code: {response.status_code}")
+            print(response.json())
+    else:
+        print(f"Error: Unable to fetch source file. Status code: {source_file_response.status_code}")
+
+push_file_to_branch(
+    'Anilathmacloudeqs',
+    'mergetest',
+    'main',
+    'release',
+    'hello.py',
+    'Commit message'
+)
