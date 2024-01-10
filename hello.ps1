ae2094056f0805c5d4408a68785ebdf918309b43
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2024-01-10 19:38:07 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,13 +1,13 @@
 # Set GitHub repository details
 $githubUsername = "Anilathmacloudeqs"
 $repository = "mergetest"
-$patToken = $env:INPUT_PAT_TOKEN  # Retrieve the access token from the repository secrets
+$patToken = $env:PAT_TOKEN  # Retrieve the access token from the repository secrets
 
 # Create the GitHub repository URL
 $repoUrl = "https://github.com/$githubUsername/$repository"
 
 # Create the API URL to list files in the repository
-$apiUrl = "$repoUrl/contents"
+$apiUrl = "https://api.github.com/repos/$githubUsername/$repository/contents"
 
 # Make a request to GitHub API to get the list of files
 $headers = @{
@@ -15,11 +15,16 @@ $headers = @{
     Accept = "application/vnd.github.v3+json"
 }
 
-$response = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get
-
-# Print the names of all files in the repository
-foreach ($file in $response) {
-    if ($file.type -eq "file") {
-        Write-Output $file.name
+try {
+    $response = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get
+    # Print the names of all files in the repository
+    foreach ($file in $response) {
+        if ($file.type -eq "file") {
+            Write-Output $file.name
+        }
     }
+}
+catch {
+    Write-Error "Error accessing GitHub API: $_"
+    throw
 }
